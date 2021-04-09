80dd6080153b9000d0598e39db18530aebd9f657
sonhai32 <lamsonhai1379@gmail.com>
2021-04-09 09:08:55 +0700

    create BUS_admin

---
+++ BUS/BUS_Admin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+namespace BUS
+{
+    public class BUS_Admin
+    {
+        public DataTable login(DTO.DTO_Admin user)
+        {
+            return new DAL.DAL_Admin().login(user);
+        }
+
+        public bool changePassword(DTO.DTO_Admin user)
+        {
+            return new DAL.DAL_Admin().changeAdminPass(user);
+        }
+    }
+}
