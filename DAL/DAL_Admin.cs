a7c161422e2357d138025a9ef12f86cfb3b6c2bc
sonhai32 <lamsonhai1379@gmail.com>
2021-04-09 08:11:17 +0700

    create DAL_Admin

---
+++ DAL/DAL_Admin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using DTO;
+namespace DAL
+{
+    public class DAL_Admin
+    {
+        private SqlCommand getCommand(string commandText)
+        {
+            SqlConnection con = DAL.DAL_DB_Connect.connection;
+            return  new SqlCommand(commandText, con);
+        }
+        public SqlDataReader login(DTO.DTO_Admin user)
+        {
+            try
+            {
+                SqlCommand command = getCommand("adminLogin");
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                command.Parameters.Add("username", System.Data.SqlDbType.VarChar).Value = user.UserName;
+                command.Parameters.Add("password", System.Data.SqlDbType.VarChar).Value = user.UserPass;
+
+                return command.ExecuteReader();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public bool changeAdminPass(DTO.DTO_Admin user)
+        {
+            try
+            {
+                SqlCommand command = getCommand("adminChangePass");
+                command.CommandType = System.Data.CommandType.StoredProcedure;
+
+                command.Parameters.Add("userID", System.Data.SqlDbType.VarChar).Value = user.UserID;
+                command.Parameters.Add("newPassword", System.Data.SqlDbType.VarChar).Value = user.UserPass;
+
+                return command.ExecuteNonQuery() >= 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
