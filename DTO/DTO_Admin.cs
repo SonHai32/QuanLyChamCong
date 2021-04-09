c8f8e898be549b9b2d34f82343c97c9645e6133b
sonhai32 <lamsonhai1379@gmail.com>
2021-04-09 07:42:17 +0700

    create DTO_admin

---
+++ DTO/DTO_Admin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    public class DTO_Admin
+    {
+        private string userID;
+        private string userName;
+        private string userPass;
+
+        public DTO_Admin(string userID, string userName, string userPass)
+        {
+            this.UserID = userID;
+            this.UserName = userName;
+            this.UserPass = userPass;
+        }
+
+        public string UserID { get => userID; set => userID = value; }
+        public string UserName { get => userName; set => userName = value; }
+        public string UserPass { get => userPass; set => userPass = value; }
+    }
+}
