6501946f726180120322fca11cc31cdfc9d88da6
sonhai32 <lamsonhai1379@gmail.com>
2021-04-09 08:55:10 +0700

    create BUS_FingerPrint

---
+++ BUS/BUS_FingerPrint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BUS
+{
+    public class BUS_FingerPrint
+    {
+        public bool insertData(DTO.DTO_FingerPrint fing)
+        {
+            return new DAL.DAL_FingerPrint().insertData(fing);
+        }
+        public bool updateData(DTO.DTO_FingerPrint fing)
+        {
+            return new DAL.DAL_FingerPrint().updateData(fing);
+        }
+        public bool deleteData(string fingID)
+        {
+            return new DAL.DAL_FingerPrint().deleteData(fingID);
+        }
+    }
+}
