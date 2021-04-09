96b89e2b2a5e2cc0edbbbabb72c32543961af7a9
sonhai32 <lamsonhai1379@gmail.com>
2021-04-09 08:43:36 +0700

    update BUS_Staff

--- BUS/BUS_Staff.cs
+++ BUS/BUS_Staff.cs
@@ -1,8 +1,21 @@
 ﻿using System;
-
+using DAL;
+using DTO;
 namespace BUS
 {
     public class BUS_Staff
     {
+        public bool insertData(DTO.DTO_Staff staff)
+        {
+            return new DAL.DAL_Staff().insertData(staff);
+        }
+        public bool updateData(DTO.DTO_Staff staff)
+        {
+            return new DAL.DAL_Staff().updateData(staff);
+        }
+        public bool deleteData(string staffID)
+        {
+            return new DAL.DAL_Staff().DeleteStaffData(staffID);
+        }
     }
 }
