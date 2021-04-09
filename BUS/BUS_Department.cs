8e686a8a16f112fd0b9e214b7911a51d5120be18
sonhai32 <lamsonhai1379@gmail.com>
2021-04-09 08:52:35 +0700

    create create BUS_Department

---
+++ BUS/BUS_Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BUS
+{
+    public class BUS_Department
+    {
+        public bool insertData(DTO.DTO_Department dept)
+        {
+            return new DAL.DAL_Department().insertData(dept);
+        }
+        public bool updateData(DTO.DTO_Department dept)
+        {
+            return new DAL.DAL_Department().updateData(dept);
+        }
+        public bool deleteData(string deptID)
+        {
+            return new DAL.DAL_Department().deleteData(deptID);
+        }
+    }
+}
