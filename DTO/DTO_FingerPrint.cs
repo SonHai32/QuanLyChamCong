019e9f9b8ffe02b8cf1c2013ddb8798d1916c498
sonhai32 <lamsonhai1379@gmail.com>
2021-04-09 07:40:35 +0700

    create DTO_Fingerprint

---
+++ DTO/DTO_FingerPrint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    public class DTO_FingerPrint
+    {
+        private string figID;
+        private string fingerPrint;
+        private string staffID;
+
+        public DTO_FingerPrint(string figID, string fingerPrint, string staffID)
+        {
+            this.FigID = figID;
+            this.FingerPrint = fingerPrint;
+            this.StaffID = staffID;
+        }
+
+        public string FigID { get => figID; set => figID = value; }
+        public string FingerPrint { get => fingerPrint; set => fingerPrint = value; }
+        public string StaffID { get => staffID; set => staffID = value; }
+    }
+}
