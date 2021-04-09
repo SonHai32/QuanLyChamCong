bcbc0d04d3c38d777be6d538c2acc9df12e2d3ca
sonhai32 <lamsonhai1379@gmail.com>
2021-04-09 08:49:05 +0700

    create BUS_Attendance

---
+++ BUS/BUS_Attendance.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BUS
+{
+    public class BUS_Attendance
+    {
+        public bool insertData(DTO.DTO_Attendance attd)
+        {
+            return new DAL.DAL_Attendance().insertData(attd);
+        }
+        public bool updateData(DTO.DTO_Attendance attd)
+        {
+            return new DAL.DAL_Attendance().updateData(attd);
+        }
+        public bool deleteData(string attdID)
+        {
+            return new DAL.DAL_Attendance().deleteData(attdID);
+        }
+    }
+}
