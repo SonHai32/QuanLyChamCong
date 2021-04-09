5f0260939faec8db6e5ccf4488be42edc364cff5
sonhai32 <lamsonhai1379@gmail.com>
2021-04-09 07:38:41 +0700

    create DTO_Department

--- DAL/DAL_Attendance.cs
+++ DAL/DAL_Attendance.cs
@@ -50,7 +50,6 @@ namespace DAL
             }
             catch
             {
-
                 return false;
             }
         }
---
+++ DTO/DTO_Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DTO
+{
+    public class DTO_Department
+    {
+        private string deptID;
+        private string deptName;
+
+        public DTO_Department(string deptID, string deptName)
+        {
+            this.DeptID = deptID;
+            this.DeptName = deptName;
+        }
+
+        public string DeptID { get => deptID; set => deptID = value; }
+        public string DeptName { get => deptName; set => deptName = value; }
+    }
+}
