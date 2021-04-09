7bea7dfdf3b8a3179431598f0519b681e3190d4d
sonhai32 <lamsonhai1379@gmail.com>
2021-04-09 08:46:07 +0700

    create BUS_Office

---
+++ BUS/BUS_Office.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL;
+using DTO;
+
+namespace BUS
+{
+    public class BUS_Office
+    {
+        public bool insertData(DTO.DTO_Office office)
+        {
+            return new DAL.DAL_Office().insertData(office);
+        }
+        public bool updateData(DTO.DTO_Office office)
+        {
+            return new DAL.DAL_Office().updateData(office);
+        }
+        public bool deleteData(string offID)
+        {
+            return new DAL.DAL_Office().deleteData(offID);
+        }
+    }
+}
